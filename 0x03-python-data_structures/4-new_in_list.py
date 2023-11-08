f454f3206531ecd64e48592402f3277e80739a63
Eric Ansah <ericbarrystilesansah@gmail.com>
2023-11-08 14:26:43 -0800

    task 4 to 5

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, new_element):
+    if idx < 0:
+        return my_list
+    elif idx >= len(my_list):
+        return my_list
+    new_list = list(my_list)
+    new_list[idx] = new_element
+    return new_listi
